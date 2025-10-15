d0f196be820fea059384d2a3262e958c3cc9e0d8
Robuit <24012645@student.hhs.nl>
2025-10-15 10:20:15 +0200

    up to date

--- TreeMarket_Klas4_Groep7/Controllers/BlogController.cs
+++ TreeMarket_Klas4_Groep7/Controllers/BlogController.cs
@@ -1,4 +1,77 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using TreeMarket_Klas4_Groep7.Data;
+//using System.Linq;
+
+//namespace TreeMarket_Klas4_Groep7.Controllers
+//{
+//    [ApiController]
+//    [Route("api/[controller]")]
+//    public class BlogController : ControllerBase
+//    {
+//        private readonly BloggingContext _context;
+
+//        public BlogController(BloggingContext context)
+//        {
+//            _context = context;
+//        }
+
+//        // GET: api/blog
+//        [HttpGet]
+//        public IActionResult GetAllProducts()
+//        {
+//            var producten = _context.Producten.ToList();
+//            return Ok(producten);
+//        }
+
+//        // POST: api/blog
+//        [HttpPost]
+//        public IActionResult AddProduct(Product product)
+//        {
+//            _context.Producten.Add(product);
+//            _context.SaveChanges();
+//            return CreatedAtAction(nameof(GetAllProducts), new { id = product.ID }, product);
+//        }
+
+//        // PUT: api/blog/5
+//        [HttpPut("{id}")]
+//        public IActionResult UpdateProduct(int id, Product updated)
+//        {
+//            var existing = _context.Producten.Find(id);
+//            if (existing == null)
+//                return NotFound();
+
+//            // velden bijwerken
+//            existing.Foto = updated.Foto;
+//            existing.Artikelenkenmerken = updated.Artikelenkenmerken;
+//            existing.Hoeveelheid = updated.Hoeveelheid;
+//            existing.MinimumPrijs = updated.MinimumPrijs;
+//            existing.LeverancierID = updated.LeverancierID;
+//            existing.Dagdatum = updated.Dagdatum;
+
+//            _context.SaveChanges();
+//            return NoContent();
+//        }
+
+//        // DELETE: api/blog/5
+//        [HttpDelete("{id}")]
+//        public IActionResult DeleteProduct(int id)
+//        {
+//            var product = _context.Producten.Find(id);
+//            if (product == null)
+//                return NotFound();
+
+//            _context.Producten.Remove(product);
+//            _context.SaveChanges();
+//            return NoContent();
+//        }
+//    }
+//}
+
+
+
+
+
+using Microsoft.AspNetCore.Mvc;
 using TreeMarket_Klas4_Groep7.Data;
 
 namespace TreeMarket_Klas4_Groep7.Controllers
@@ -27,6 +100,28 @@ namespace TreeMarket_Klas4_Groep7.Controllers
             _context.Blogs.Add(blog);
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetAllBlogs), new { id = blog.BlogId }, blog);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlog(int id, Blog updatedBlog)
+        {
+            var blog = _context.Blogs.Find(id);
+            if (blog == null) return NotFound();
+
+            blog.Url = updatedBlog.Url;
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlog(int id)
+        {
+            var blog = _context.Blogs.Find(id);
+            if (blog == null) return NotFound();
+
+            _context.Blogs.Remove(blog);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
--- TreeMarket_Klas4_Groep7/Data/BloggingContext.cs
+++ TreeMarket_Klas4_Groep7/Data/BloggingContext.cs
@@ -1,4 +1,53 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿//using Microsoft.EntityFrameworkCore;
+
+//namespace TreeMarket_Klas4_Groep7.Data
+//{
+//    public class BloggingContext : DbContext
+//    {
+//        public BloggingContext(DbContextOptions<BloggingContext> options)
+//            : base(options) { }
+
+//        public DbSet<Gebruiker> Gebruikers { get; set; }
+//        public DbSet<Klant> Klanten { get; set; }
+//        public DbSet<Leverancier> Leveranciers { get; set; }
+//        public DbSet<Veilingsmeester> Veilingsmeesters { get; set; }
+//        public DbSet<Product> Producten { get; set; }
+//        public DbSet<Veiling> Veilingen { get; set; }
+//        public DbSet<Claim> Claims { get; set; }
+//        public DbSet<Dashboard> Dashboards { get; set; }
+
+//        protected override void OnModelCreating(ModelBuilder b)
+//        {
+//            // --- 1↔1 Gebruiker <-> Klant ---
+//            b.Entity<Klant>().HasKey(k => k.KlantID);
+//            b.Entity<Klant>()
+//                .HasOne(k => k.Gebruiker)
+//                .WithOne(g => g.Klant)
+//                .HasForeignKey<Klant>(k => k.KlantID)
+//                .OnDelete(DeleteBehavior.Cascade);
+
+//            // --- 1↔1 Gebruiker <-> Leverancier ---
+//            b.Entity<Leverancier>().HasKey(l => l.LeverancierID);
+//            b.Entity<Leverancier>()
+//                .HasOne(l => l.Gebruiker)
+//                .WithOne(g => g.Leverancier)
+//                .HasForeignKey<Leverancier>(l => l.LeverancierID)
+//                .OnDelete(DeleteBehavior.Cascade);
+
+//            // --- 1↔1 Gebruiker <-> Veilingsmeester ---
+//            b.Entity<Veilingsmeester>().HasKey(v => v.VeilingsmeesterID);
+//            b.Entity<Veilingsmeester>()
+//                .HasOne(v => v.Gebruiker)
+//                .WithOne(g => g.Veilingsmeester)
+//                .HasForeignKey<Veilingsmeester>(v => v.VeilingsmeesterID)
+//                .OnDelete(DeleteBehavior.Cascade);
+//        }
+//    }
+//}
+
+
+
+using Microsoft.EntityFrameworkCore;
 
 namespace TreeMarket_Klas4_Groep7.Data
 {
