e0182b99b2e6e5ee8e56624d1332848b1cf18d28
DannyNguyen0411 <90318109+DannyNguyen0411@users.noreply.github.com>
2025-10-30 07:47:09 +0100

    Made some changes, but didn't pushed yesterday

--- TreeMarket_Klas4_Groep7/Controllers/GebruikerController.cs
+++ TreeMarket_Klas4_Groep7/Controllers/GebruikerController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TreeMarket_Klas4_Groep7.Data;
+using TreeMarket_Klas4_Groep7.Views;
 
 namespace TreeMarket_Klas4_Groep7.Controllers
 {
@@ -7,6 +9,30 @@ namespace TreeMarket_Klas4_Groep7.Controllers
     [ApiController]
     public class GebruikerController : ControllerBase
     {
+        private readonly ApiContext _context;
+
+        public GebruikerController(ApiContext context)
+        {
+            _context = context;
+        }
+
         //Mogelijk voor sub klasses, of het moet apart😝
+        [HttpPost]
+        public JsonResult CreateGebruiker(Gebruiker gebruiker)
+        {
+            if (gebruiker.Id == 0) { 
+                _context.Gebruiker.Add(gebruiker);
+            }
+            else
+            {
+                var gebruikerInDb = _context.Gebruiker.Find(gebruiker.Id);
+
+                if(gebruikerInDb == null) 
+                {
+                    return new JsonResult(NotFound());
+                }
+            }
+            return new JsonResult(Ok());
+        }
     }
 }
