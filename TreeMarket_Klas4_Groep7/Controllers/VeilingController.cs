9a707e14160e9e857c4b31ff93c7f80615cf076d
Hamza Harsal <hamzaharsal10@gmail.com>
2025-11-12 10:15:55 +0100

    d

--- TreeMarket_Klas4_Groep7/Controllers/VeilingController.cs
+++ TreeMarket_Klas4_Groep7/Controllers/VeilingController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using SQLitePCL;
 using TreeMarket_Klas4_Groep7.Data;
-using TreeMarket_Klas4_Group7.Models;
 
 namespace TreeMarket_Klas4_Group7.Controllers
 {
