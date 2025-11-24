c415ae18491378cdabbba43d8cf120ea930c750c
DannyNguyen0411 <90318109+DannyNguyen0411@users.noreply.github.com>
2025-11-24 12:08:49 +0100

    Created a test project. It has to be used with DTO

---
+++ WebAPI.Tests/Gebruiker/GebruikerTonenTest.cs
@@ -0,0 +1,25 @@
+﻿//using Xunit;
+//using WebAPI.Controllers;
+//using Microsoft.AspNetCore.Mvc;
+//using System.Collections.Generic;
+
+//namespace WebAPI.Tests
+//{
+//    public class GebruikerControllerTests
+//    {
+//        [Fact]
+//        public void GetAll_ReturnsOkResult_WithListOfUsers()
+//        {
+//            // Arrange
+//            var controller = new GebruikerController();
+
+//            // Act
+//            var result = controller.GetAll();
+
+//            // Assert
+//            var okResult = Assert.IsType<OkObjectResult>(result);
+//            var users = Assert.IsType<List<string>>(okResult.Value); // pas type aan naar jouw return type
+//            Assert.NotEmpty(users);
+//        }
+//    }
+//}
