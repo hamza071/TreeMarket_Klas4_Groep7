01175a93b5f98d559e29f39f287219c9f409cd44
Robuit <24012645@student.hhs.nl>
2025-12-09 16:08:38 +0100

    a

--- frontend/src/pages/CRUD/AllUsers.jsx
+++ frontend/src/pages/CRUD/AllUsers.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-// We importeren de authFetch zodat we de token mee kunnen geven 
+// 1. Importeer je slimme functie
 import { authFetch } from "../../apiClient";
 function AllUsers() {
     const [users, setUsers] = useState([]);
@@ -9,7 +9,7 @@ function AllUsers() {
         // We maken een async functie voor de aanroep
         const fetchUsers = async () => {
             try {
-             
+
                 const data = await authFetch("/Gebruiker/GetAllUsers", {
                     method: "GET"
                 });
@@ -24,29 +24,29 @@ function AllUsers() {
         fetchUsers();
     }, []);
 
-    if (error) return <div style={{color: "red"}}>Error: 404. U heeft geen rechten tot deze pagina</div>;
+    if (error) return <div style={{ color: "red" }}>Error: 404. U heeft geen rechten tot deze pagina</div>;
 
     return (
         <div>
             <h2>Alle Gebruikers</h2>
             <table border="5" cellPadding="5" cellSpacing="5">
                 <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Naam</th>
-                    <th>Email</th>
-                    <th>Rol</th>
-                </tr>
+                    <tr>
+                        <th>ID</th>
+                        <th>Naam</th>
+                        <th>Email</th>
+                        <th>Rol</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {users.map(user => (
-                    <tr key={user.gebruikerId}>
-                        <td>{user.gebruikerId}</td>
-                        <td>{user.naam}</td>
-                        <td>{user.email}</td>
-                        <td>{user.rol}</td>
-                    </tr>
-                ))}
+                    {users.map(user => (
+                        <tr key={user.gebruikerId}>
+                            <td>{user.gebruikerId}</td>
+                            <td>{user.naam}</td>
+                            <td>{user.email}</td>
+                            <td>{user.rol}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
