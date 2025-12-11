0bfd45e68e32e575e8897ab861e408ccfa4a9273
Hamza Harsal <hamzaharsal10@gmail.com>
2025-12-11 02:16:19 +0100

    fix hopelijk

---
+++ backend/Services/ClaimService.cs
@@ -0,0 +1,52 @@
+﻿using backend.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+using backend.Models;
+using backend.Models.DTO;
+
+namespace backend.Services
+{
+    public class ClaimService : IClaimController
+    {
+        private readonly ApiContext _context;
+
+        public ClaimService(ApiContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Claim>> GetClaimsAsync()
+        {
+            return await _context.Claim
+                .Include(c => c.klant)
+                .Include(c => c.Veiling)
+                .ToListAsync();
+        }
+
+        public async Task<Claim> CreateClaimAsync(ClaimDto dto, string userId)
+        {
+            var claim = new Claim
+            {
+                Prijs = dto.prijs,
+                VeilingId = dto.veilingId,
+                KlantId = userId   // van de token!
+            };
+
+            await _context.Claim.AddAsync(claim);
+            await _context.SaveChangesAsync();
+
+            return claim;
+        }
+
+        public async Task<bool> DeleteClaimAsync(int id)
+        {
+            var claim = await _context.Claim.FindAsync(id);
+            if (claim == null) return false;
+
+            _context.Claim.Remove(claim);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
