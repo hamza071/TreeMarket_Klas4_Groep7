0ad93a965961c99e93e2a761b09b4569dc9fbb77
Hamza Harsal <hamzaharsal10@gmail.com>
2025-12-11 02:11:20 +0100

    s

---
+++ backend/Interfaces/IProductController.cs
@@ -0,0 +1,14 @@
+﻿using backend.Models;
+using backend.Models.DTO;
+
+namespace backend.Interfaces
+{
+    public interface IProductController
+    {
+        Task<List<ProductDto>> GetProductenVanVandaagAsync();
+        Task<List<ProductMetLeverancierDto>> GetProductenMetLeverancierAsync();
+        Task<Product> AddOrUpdateProductAsync(Product product);
+        Task<Product> GetByIdAsync(int productId);
+        Task<bool> DeleteAsync(int productId);
+    }
+}
---
+++ backend/Interfaces/IVeilingController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using backend.Models;
+using backend.Models.DTO;
+
+namespace backend.Interfaces
+{
+    public interface IVeilingController
+    {
+        Task<Veiling> CreateVeilingAsync(VeilingDto dto, string userId);
+        
+        Task<List<Veiling>> GetAllAsync();
+        Task<Veiling> GetByIdAsync(int veilingId);
+        Task<Bid> PlaceBidAsync(CreateBidDTO dto, string userId);
+        Task<Veiling> UpdateStatusAsync(int veilingId, bool status);
+    }
+}
