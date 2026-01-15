8956a4ca43253ab8f143814cbfbf6288eb173083
Hamza Harsal <61660867+hamza071@users.noreply.github.com>
2026-01-15 01:13:28 +0100

    Laatste

--- frontend/src/DeployLocal.js
+++ frontend/src/DeployLocal.js
@@ -1,5 +1,5 @@
 // frontend/src/DeployLocal.js
 
 // Dit pakt de Vercel-link (via de environment variable), 
-// OF als die leeg is (op je eigen PC), pakt hij localhost.
-export const API_URL = import.meta.env.VITE_API_URL || '${API_URL}';
+// OF als die leeg is (op je eigen PC), pakt hij jouw lokale backend.
+export const API_URL = import.meta.env.VITE_API_URL || 'https://localhost:7054';
